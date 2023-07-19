a6f3b5046806de63aa46a7bd1f5d244e717e8598
MARTINS, Felipe <Felipemartins.fab@gmail.com>
2023-07-19 17:57:24 -0300

    Conclusão do CRUD

--- src/main/java/br/com/farmacia/bean/FornecedoresBean.java
+++ src/main/java/br/com/farmacia/bean/FornecedoresBean.java
@@ -6,16 +6,20 @@ import java.util.ArrayList;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.model.ListDataModel;
 
 import br.com.farmacia.dao.FornecedoresDAO;
 import br.com.farmacia.domain.Fornecedores;
+import br.com.farmacia.util.JSFUtil;
 
 @ManagedBean(name = "MBFornecedores")
 @ViewScoped
 public class FornecedoresBean {
 
 	private Fornecedores fornecedores;
+
+	private ArrayList<Fornecedores> itens;
+
+	private ArrayList<Fornecedores> itensFiltrados;
 
 	public Fornecedores getFornecedores() {
 		return fornecedores;
@@ -25,28 +29,32 @@ public class FornecedoresBean {
 		this.fornecedores = fornecedores;
 	}
 
-	private ListDataModel<Fornecedores> itens;
-
-	public ListDataModel<Fornecedores> getItens() {
+	public ArrayList<Fornecedores> getItens() {
 		return itens;
 	}
 
-	public void setItens(ListDataModel<Fornecedores> itens) {
+	public void setItens(ArrayList<Fornecedores> itens) {
 		this.itens = itens;
+	}
+
+	public ArrayList<Fornecedores> getItensFiltrados() {
+		return itensFiltrados;
+	}
+
+	public void setItensFiltrados(ArrayList<Fornecedores> itensFiltrados) {
+		this.itensFiltrados = itensFiltrados;
 	}
 
 	@PostConstruct
 	public void prepararPesquisa() {
 
-		FornecedoresDAO fdao = new FornecedoresDAO();
-
 		try {
 
-			ArrayList<Fornecedores> lista = fdao.listar();
-			itens = new ListDataModel<Fornecedores>(lista);
+			FornecedoresDAO fdao = new FornecedoresDAO();
+			itens = fdao.listar();
 
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			JSFUtil.adicionarMsgErro("ex.getMessage()");
 			e.printStackTrace();
 		}
 
@@ -60,12 +68,53 @@ public class FornecedoresBean {
 
 	public void novo() {
 
-		FornecedoresDAO fdao = new FornecedoresDAO();
+		try {
+
+			FornecedoresDAO fdao = new FornecedoresDAO();
+			fdao.salvar(fornecedores);
+
+			itens = fdao.listar();
+
+			JSFUtil.adicionarMsgSucesso("Salvo com sucesso");
+
+		} catch (SQLException e) {
+			JSFUtil.adicionarMsgErro("ex.getMessage()");
+			e.printStackTrace();
+		}
+
+	}
+
+	public void excluir() {
 
 		try {
-			fdao.salvar(fornecedores);
+
+			FornecedoresDAO fdao = new FornecedoresDAO();
+			fdao.excluir(fornecedores);
+
+			itens = fdao.listar();
+
+			JSFUtil.adicionarMsgSucesso("Excluído com sucesso");
+
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			JSFUtil.adicionarMsgErro("Erro ao excluir o Fornecedor");
+			e.printStackTrace();
+		}
+
+	}
+
+	public void editar() {
+
+		try {
+
+			FornecedoresDAO fdao = new FornecedoresDAO();
+			fdao.editar(fornecedores);
+
+			itens = fdao.listar();
+
+			JSFUtil.adicionarMsgSucesso("Edição com sucesso");
+
+		} catch (SQLException e) {
+			JSFUtil.adicionarMsgErro("Erro ao editar o Fornecedor");
 			e.printStackTrace();
 		}
 
